948c2cb705fe3708c9d26b2221a62d6160d54bf4
Industria 4.0 <ania.escudero@opendeusto.es>
2022-04-12 18:40:58 +0200

    Añadir clase tares y proyecto

--- appDeustotil/models.py
+++ appDeustotil/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Tarea(models.Model):
+    Nombre = models.CharField()
+    Descripcion = models.CharField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    responsable = models.ForeignKey(Empleado)
+    prioridad = models.CharField()
+    estado = models.CharField()
+    notas = models.CharField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Proyecto(models.Model):
+    Nombre = models.CharField()
+    Descripcion = models.CharField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    Presupuesto = models.IntegerField()
+    Cliente = models.ManyToManyField(Cliente)
+    Tareas = models.ForeignKey(Tarea)
+    Reponsable = models.ForeignKey(Empleado)
+
+    def __str__(self):
+        return self.Nombre
+
