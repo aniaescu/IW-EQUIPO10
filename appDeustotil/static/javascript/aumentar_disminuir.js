2f4abde183a4060071fdc50fea806acc4bedce70
Xabi <xabiochoa@opendeusto.es>
2022-05-31 23:52:16 +0200

    comentarios

--- appDeustotil/static/javascript/aumentar_disminuir.js
+++ appDeustotil/static/javascript/aumentar_disminuir.js
@@ -1,6 +1,8 @@
+//array que definen en el estilos.css el tamaño de la letra y el índice
 var classes = ["f0", "f1", "f2", "f3", "f4", "f5", "f6"];
 var classIndex = 2;
 
+//manejador de eventos que se encargan de asociar las funcionalidades a los botones, en este caso aumentar tamaño
 document.getElementById('aumentar').addEventListener('click', function () {
     let previousClass = classIndex;
     classIndex++;
@@ -8,6 +10,7 @@ document.getElementById('aumentar').addEventListener('click', function () {
     changeClass(previousClass, classIndex);
 });
 
+//manejador de eventos que se encargan de asociar las funcionalidades a los botones, en este caso disminuir tamaño
 document.getElementById('disminuir').addEventListener('click', function () {
     let previousClass = classIndex;
     classIndex--;
@@ -15,6 +18,7 @@ document.getElementById('disminuir').addEventListener('click', function () {
     changeClass(previousClass, classIndex);
 });
 
+//funcion que sirve para comprobar que el nombre de la clase ha cambiado
 function changeClass(previous, next) {
     if (previous != next) {
         var htmlElement = document.querySelector('html')
